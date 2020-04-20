603ab72832dfe921fa33e6d93af582f9f4038690
Brent <kb1lqd@gmail.com>
2020-04-19 22:31:09 -0700

    Working query of MCP9808 registers including temperature value.

--- I2C/mcp9808_test.c
+++ I2C/mcp9808_test.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <bcm2835.h>
 
-#define SLAVE_ADDRESS 0x18
+#define SLAVE_ADDRESS 24 //0x18
 #define BUF_SIZE 4
 
 int main (void)
@@ -33,24 +33,30 @@ int main (void)
 	printf("Initializing MCP9808.\n");
 
 	//initialize I2C pins for I2C operations (ALT mode)
-	bcm2835_i2c_begin();
+	if(!bcm2835_i2c_begin())
+	{
+		printf("I2C Init Failed. Are you running as root?");
+	}
 	
 	printf("Step 1\n");
 	
 	//set slave address
-	bcm2835_i2c_setSlaveAddress(SLAVE_ADDRESS);
+	bcm2835_i2c_setSlaveAddress(24);
 
 	printf("Step 2\n");
 
 	
 	//set baudrate (standard is 100kbaud)
-	bcm2835_i2c_setClockDivider(clk_div);
+	//bcm2835_i2c_setClockDivider(clk_div);
     fprintf(stderr, "Clock divider set to: %d\n", clk_div);
-	//bcm2835_i2c_set_baudrate(100000);
+	bcm2835_i2c_set_baudrate(100000);
 	
 	printf("Reading Device %d Register %d\n", SLAVE_ADDRESS, current_reg);
+	
+	char tempwr[1] = {5};
 
-	mcp9808_reason_code = bcm2835_i2c_read(rxbuf, 4);
+	bcm2835_i2c_write(tempwr, 1);
+	mcp9808_reason_code = bcm2835_i2c_read(rxbuf, 2);
 	printf("REASON CODE: %d\n", mcp9808_reason_code);
 	
 	for(i=0;i<BUF_SIZE;i++)
